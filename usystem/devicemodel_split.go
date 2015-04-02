893e7718fa1c9ff9011f27f2eb71d4b7cb86de8b
Dmitry S. Fedorov <d.fedorov@naukanet.ru>
2015-04-02 14:03:25 +0300

    Пропустил через gofmt

--- usystem/devicemodel_split.go
+++ usystem/devicemodel_split.go
@@ -8,21 +8,21 @@ import "encoding/csv"
 func main() {
 	path_to_file := "/home/fedusia/devicemodel.csv"
 	file, err := os.Open(path_to_file)
-	
+
 	if err != nil {
 		log.Fatalln(err)
 	}
 	defer file.Close()
 	r := csv.NewReader(file)
 	lines, err := r.ReadAll()
-	
+
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	id:=21
+	id := 21
 	for _, value := range lines {
-		fmt.Printf ("INSERT INTO \"devicemodel\" VALUES(%[1]d,'%[2]s', %[3]s, '%[4]s', %[5]s, '%[6]s', '%[7]s', '%[8]s');\n",id, value[0], value[1], value[2], value[3], value[4], value[5], value[6])
+		fmt.Printf("INSERT INTO \"devicemodel\" VALUES(%[1]d,'%[2]s', %[3]s, '%[4]s', %[5]s, '%[6]s', '%[7]s', '%[8]s');\n", id, value[0], value[1], value[2], value[3], value[4], value[5], value[6])
 		id++
-	} 
+	}
 }
--- usystem/tmp.go
+++ usystem/tmp.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	
-	for id:=21; id <= 68; id++{
+
+	for id := 21; id <= 68; id++ {
 		fmt.Printf("Delete from devicemodel where id=%d;\n", id)
 	}
 }
