f4e652f13db9afdff420b08b7ae68d5fc28e20fa
Dmitry S. Fedorov <d.fedorov@naukanet.ru>
2015-06-21 01:00:46 +0300

    task2: complete! Вторая задача далась гораздо быстрее!

--- acmp.ru/task2/main.go
+++ acmp.ru/task2/main.go
@@ -1,15 +1,25 @@
 package main
 
+/*
+Link: http://acmp.ru/?main=task&id_task=2
+Date: 21.06.2015
+*/
+
 import (
 	"fmt"
-	"log"
+	"io"
 	"io/ioutil"
+	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 var (
-	input  = "./INPUT.TXT"
-	output = "./OUTPUT.TXT"
+	input     = "./INPUT.TXT"
+	output    = "./OUTPUT.TXT"
+	bytes     = make([]byte, 100)
+	a, result int
 )
 
 func main() {
@@ -18,9 +28,20 @@ func main() {
 		log.Fatalln(err)
 	}
 	defer file.Close()
-	r,err := ioutil.ReadFile(input)
-	if err != nil {
-		log.Fatalln(err)
+
+	r := io.Reader(file)
+
+	bytes, err = ioutil.ReadAll(r)
+	line := string(bytes)
+	slice := strings.Fields(line)
+	a, err = strconv.Atoi(slice[0])
+
+	for i := 0; i <= a; i++ {
+		result += i
 	}
-	fmt.Println(string(r)) 
+
+	fmt.Println(result)
+	result := strconv.Itoa(result)
+	ioutil.WriteFile(output, []byte(result), 0644)
+
 }
