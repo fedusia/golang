2379ca7f91f0027545d87befcfc1f2463020d4a0
Dmitry S. Fedorov <d.fedorov@naukanet.ru>
2015-06-22 22:57:58 +0300

    minor fixes, deleted some useless lines

--- acmp.ru/task1/main.go
+++ acmp.ru/task1/main.go
@@ -6,10 +6,8 @@ Date: 18.06.2015
 */
 
 import (
-	"io"
 	"io/ioutil"
 	"log"
-	"os"
 	"strconv"
 	"strings"
 )
@@ -17,32 +15,30 @@ import (
 var (
 	input  = "./INPUT.TXT"
 	output = "./OUTPUT.TXT"
-	bytes  = make([]byte, 100)
-	a      int
-	b      int
-	sum    int
 )
 
 func main() {
-	file, err := os.Open(input)
+	// Читаем все байты из файла
+	bytes, err := ioutil.ReadFile(input)
 	if err != nil {
 		log.Fatalln(err)
 	}
-	defer file.Close()
-
-	r := io.Reader(file)
-	// Читаем все байты из файла
-	bytes, err := ioutil.ReadAll(r)
 	// Переводим байты в строку
 	line := string(bytes)
 	//Разбиваем по пробелам строку
 	slice := strings.Fields(line)
 	//Меняем тип с str на int
-	a, err = strconv.Atoi(slice[0])
-	b, err = strconv.Atoi(slice[1])
-	sum = a + b
+	a, err := strconv.Atoi(slice[0])
+	if err != nil {
+		log.Fatalln(err)
+	}
+	b, err := strconv.Atoi(slice[1])
+	if err != nil {
+		log.Fatalln(err)
+	}
+	sum := a + b
 	// Меняем тип с int на  str и затем конвертим в байты и записываем в файл
-	sum := strconv.Itoa(sum)
-	ioutil.WriteFile(output, []byte(sum), 0644)
+	result := strconv.Itoa(sum)
+	ioutil.WriteFile(output, []byte(result), 0644)
 
 }
