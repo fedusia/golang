6ac06831408b172bac7482b48946fb30b0d3ccba
Dmitry S. Fedorov <fedorov@corp.sputnik.ru>
2016-03-25 23:36:02 +0300

    deleted not used files

--- elastic/get.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-#/usr/bin/curl -XGET 'http://elk-102.ix.km:9200/logstash-2015.10.27/scribe_sv_maps_fullsearch_prod/1'
-/usr/bin/curl -XGET 'http://elk-102.ix.km:9200/logstash-2015.10.27/scribe_sv_maps_fullsearch_prod/_search?q=*&size=11152080'
