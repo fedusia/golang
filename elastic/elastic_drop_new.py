0b4a4a0bd9b8ca238d83aba27f24a6b17ae793ab
Dmitry S. Fedorov <fedorov@corp.sputnik.ru>
2016-03-25 23:32:41 +0300

    elastic_drop_new.py: worked script

--- elastic/elastic_drop_new.py
+++ elastic/elastic_drop_new.py
@@ -1,50 +1,72 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 import requests
+import logging
 
+###############
+# basicConfig #
+###############
+logging.basicConfig(format='%(asctime)s %(message)s', filename='/var/log/elastic_drop.log', level=logging.INFO)
+HOST = 'elk-102.ix.km'
+exludeIndicies = ['logstash-2014.05.24', 'logstash-2014.05.25', 'logstash-2014.05.27', 'logstash-2014.05.29',\
+                  'logstash-2014.05.31', 'logstash-2014.05.30', '.kibana', 'grafana-dash', 'kibana-int']
+################
 
-x = 9
-host = 'elk-102.ix.km'
+if len(exludeIndicies) == 0:
+    logging.info('exludeIndicies is empty: do nothing')
+    exit(1)
 
-response = requests.get('http://%s:9200/_cluster/health' % host)
-print response
+try:
+    response = requests.get('http://%s:9200/_cluster/health' % HOST, timeout=5)
+except requests.exceptions.Timeout as t:
+    logging.info('Connection timeout, %s' % t)
+    exit(1)
+
 if response.status_code != 200:
-	# TODO: Do error message to log file
-	print ('Wrong response, %s') % response.status_code
-	exit (1)
+    logging.info('Wrong response, %s' % response.status_code)
+    exit(1)
+logging.info('HTTP answer is: %s -> doing work' % response.status_code)
 
-print response.status_code
-
-responseBody  = response.json()
+responseBody = response.json()
 if responseBody['status'] != 'green':
-	# TODO: Do error message to log file
-	print 'Status not green do nothing'
-	exit (1)
+    logging.info('Cluster status is not green: %s -> stopped' % responseBody['status'])
+    exit(1)
+logging.info('cluster Status is: %s -> doing work' % responseBody['status'])
 
-print ('Cluster Status is: %s') % responseBody['status']
+try:
+    response = requests.get('http://%s:9200/_cat/indices' % HOST)
+except requests.exceptions.Timeout as t:
+    logging.info('Connection timeout, %s' % t)
+    exit(1)
+if response.status_code != 200:
+    logging.info('Wrong response, %s' % response.status_code)
+    exit(1)
+logging.info('http answer code is: %s -> doing work' % response.status_code)
 
-response = requests.get('http://%s:9200/_cat/indices' % host)
-responseBody = response.text
-print type(responseBody)
-#responseBody = responseBody.split('\n')
-#print responseBody
-#for i in responseBody:
-#	array = i.split(' ')
-#	print array[1]
+responseBody = response.text.rstrip()
+responseBody = responseBody.split('\n')
+clusterIndicies = list()
+for i in responseBody:
+    k = i.split(' ')
+    clusterIndicies.append(k[2])
+clusterIndicies.sort()
 
-# i = len(text)
-#indices = []
-# while x < i:
-# indices.append(text[x])
-# x = x + 8
-# print indices
-# print len(indices)
-# llen = len(indices) - 60
-# print llen
-# if llen >= 9:
-# indices.sort()
-# print indices
-# for elem in range(9,llen):
-# print 'Delete: ', indices[elem]
-#     r = requests.delete('http://elk-102.ix.km:9200/'+indices[elem])
-#     print r
+resultIndicies = list()
+for index in clusterIndicies:
+    if index not in exludeIndicies:
+        resultIndicies.append(index)
+
+if len(resultIndicies) - 60 == 0:
+    logging.info('Nothing to delete')
+    exit(0)
+
+for index in range(len(resultIndicies) - 60):
+    logging.info('Trying to delete index: %s' % resultIndicies[index])
+    try:
+        response = requests.delete(('http://%s:9200/' % HOST)+resultIndicies[index], timeout=5)
+    except requests.exceptions.Timeout:
+        logging.info('Could not delete index, %s' % resultIndicies[index])
+    if response.status_code != 200:
+        logging.info('Could not delete index: %s httpcode: %s' % resultIndicies[index], response.status_code)
+    logging.info('Index: %s deleted' % resultIndicies[index])
