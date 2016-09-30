b4cc38faaef774961587c9acb6f22517e3b058bb
Dmitry S. Fedorov <fedusia@yandex-team.ru>
2016-09-30 18:49:40 +0300

    lineQueue: use pointer instead

--- data_structs/lineQueue.go
+++ data_structs/lineQueue.go
@@ -1,46 +1,60 @@
 package main
 
 import (
+	//	"fmt"
 	"io"
 	"log"
 	"math/rand"
 	"net/http"
+	"strings"
 )
 
 var (
 	queue []string
+	x     string
+	q     *[]string
 )
 
 func main() {
+	q = InitQueue()
+	//	http.HandleFunc("/delete", DeleteQueue)
 	http.HandleFunc("/list", ListQueue)
 	http.HandleFunc("/append", AppendQueue)
 	log.Fatal(http.ListenAndServe(":12345", nil))
 }
 
-func InitQueue() {
+func InitQueue() *[]string {
+	return &queue
+}
 
+func list(q *[]string) string {
+	return strings.Join(*q, ",")
 }
 
 func ListQueue(w http.ResponseWriter, req *http.Request) {
-	for _, v := range queue {
-		io.WriteString(w, v)
-	}
+	io.WriteString(w, list(q))
 }
 
 func AppendQueue(w http.ResponseWriter, req *http.Request) {
 	a := GenData()
-	queue = append(queue, a)
+	*q = append(*q, a)
 	io.WriteString(w, a)
-
 }
 
-/*
-func DeleteQueue(q *[]string) string {
-	x, a = a[len(a)-1], a[:len(a)-1]
-}
-*/
 func GenData() string {
 	rand.Seed(1)
 	a := []string{"a", "b", "c"}
 	return a[rand.Intn(len(a))]
 }
+
+/*
+func DeleteQueue(w http.ResponseWriter, req *http.Request) {
+	a := Remove(q)
+	io.WriteString(w, a)
+}
+
+func Remove(q *[]string) string {
+	x, q = q[len(q)-1], q[:len(q)-1]
+	return x
+}
+*/
